1e0e9da434035cf3d1ef0fa47fa53c771632b787
Nikita Sirenko <n.sirenko@runetsoft.ru>
2020-12-24 15:19:14 +0300

    Для фронтов

--- php_interface/s1/init.php
+++ php_interface/s1/init.php
@@ -1,2 +1,10 @@
 <?
 require_once(__DIR__ . '/events.php');
+
+use Bitrix\Main\Page\Asset;
+
+AddEventHandler('main', 'onProlog', function(){ 
+    
+    Asset::getInstance()->addJs("/local/js/autohint.js");
+    
+});
