2b4324c9cb705cc0004bc1ea0c1b2e8f0973f3aa
Аклексей Аскерко <askerbl67@gmail.com>
2020-12-30 15:30:07 +0300

    список, создание новой, вывод подсказок

--- php_interface/s1/init.php
+++ php_interface/s1/init.php
@@ -3,8 +3,23 @@ require_once(__DIR__ . '/events.php');
 
 use Bitrix\Main\Page\Asset;
 
-AddEventHandler('main', 'onProlog', function(){ 
-    
-    Asset::getInstance()->addJs("/local/js/autohint.js");
-    
-});
+\Bitrix\Main\UI\Extension::load("ui.hint");
+
+/* $arJsConfig = array( 
+    'customJs' => array( 
+        'js'=> '/bitrix/js/Interval/Interval.js', 
+    ) 
+); 
+
+foreach ($arJsConfig as $ext => $arExt) { 
+    \CJSCore::RegisterExt($ext, $arExt); 
+}
+ */
+CUtil::InitJSCore(array('customJs'));
+
+CJSCore::Init(array('ajax', 'popup', 'jquery'));
+
+AddEventHandler('main', 'onProlog', function(){     
+     Asset::getInstance()->addJs("/local/js/newHint.js"); 
+    Asset::getInstance()->addCss("/local/styles/style.css");
+}, 5000);
